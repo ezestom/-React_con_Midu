a596e907efb7d54dfde79a27df5d19966d449add
eze stom <ezequielstom@gmail.com>
2023-06-09 21:07:30 -0300

    finished react-prueba-tecnica class

--- 04-react-prueba-tecnica/src/App.jsx
+++ 04-react-prueba-tecnica/src/App.jsx
@@ -6,7 +6,7 @@ const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
 export function App() {
 	const [fact, setFact] = useState();
 	const [imageUrl, setImageUrl] = useState();
-
+	// para recuperar la cita al cargar la página
 	useEffect(() => {
 		fetch(CAT_ENDPOINT_RANDOM_FACT)
 			.then((res) => res.json())
@@ -14,21 +14,25 @@ export function App() {
 				console.log(data);
 				const { fact } = data;
 				setFact(fact);
-
-				const threeFirstWords = fact.split(" ", 3).join(" ");
-				console.log(threeFirstWords);
-
-				fetch(
-					`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
-				)
-					.then((res) => res.json())
-					.then((response) => {
-						console.log(response);
-						const { url } = response;
-						setImageUrl(url);
-					});
 			});
-	}, []);
+	},[]);
+
+	// para recuperar la imagen cada vez que tenemos una nueva cita
+	useEffect(() => {
+		if (!fact) return;
+		const threeFirstWords = fact.split(" ", 3).join(" ");
+		console.log(threeFirstWords);
+
+		fetch(
+			`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
+		)
+			.then((res) => res.json())
+			.then((response) => {
+				console.log(response);
+				const { url } = response;
+				setImageUrl(url);
+			});
+	}, [fact]);
 
 	return (
 		<main>
